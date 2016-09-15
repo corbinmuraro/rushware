9c9208b0ace8d9e9697f5b0ea9e4e96aad0e19e1
Andrew Lee <git@drewlee.com>
2016-09-14 18:02:09 -0700

    Add text on index

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+get '/' do
+  '<h2>These are proof of concepts. They are not functioning yet.</h1>
+  <p><a href="/signin">Sign In Page</a>: what rushees will see</p>
+  <p><a href="/rushee">Rushee Page</a>: example rushee profile page</p>
+  <p><a href="/rushees">Rushees Page</a>: all of the rushees</p>
+  <p>The plan is to make sign in capture photo + name so that after each rush event,
+  we can quickly go through people we met and fill out our thoughts.</p>'
+end
+
 get '/signin' do
   send_file 'signin.html'
 end
