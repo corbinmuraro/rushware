71f7b267b8eeb46278e4e34ea88c7b10fdca8df5
Andrew Lee <git@drewlee.com>
2016-09-21 23:52:26 -0700

    Generate Applicant scaffold

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :applicants
   get 'signin', to: 'signin#index'
   get 'signin/go'
 
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915231336) do
+ActiveRecord::Schema.define(version: 20160922065100) do
+
+  create_table "applicants", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
