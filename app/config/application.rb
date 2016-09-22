0734ddacc8793a21da701f410033755b887f62df
Andrew Lee <git@drewlee.com>
2016-09-22 00:05:03 -0700

    Disable assets generation for scaffolds

--- app/config/application.rb
+++ app/config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     config.generators.jbuilder = false
+    config.generators.assets = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
